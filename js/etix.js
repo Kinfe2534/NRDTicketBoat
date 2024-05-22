0fffed9a293381dbfa56c5d890f8917207fbc9bd
Kinfe Desta <Kinfe2534@gmail.com>
2024-05-22 14:24:26 +0300

    no_full_page branch update with etix

--- js/etix.js
+++ js/etix.js
@@ -18,7 +18,7 @@ async function get_confirmation_data_etix() {
       credentials: "include",
       method: "GET",
       headers: {
-        Accept: "/",
+        Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Accept-Encoding": "gzip, deflate, br, zstd",
         "Content-Type": "text/plain;charset=UTF-8",
         "accept-language": "en-US,en;q=0.9",
